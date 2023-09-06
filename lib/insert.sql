4df4c6f0bd0d4418c1c302dfad7c9583ca1324ce
Shania Lopez <slopez@fandm.edu>
2023-09-05 20:51:33 -0400

    complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 22, "F", "Brown", "angry", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 23, "M", "Black", "sassy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 24, "F", "Blue", "mean", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 25, "White", "Brown", "happy", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 26, "F", "Dark brown", "super mad", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 27, "M", "Red", "weird", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wnedy", 28, "F", "Brown", "mellow", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (Null, 29, "F", "Brown", "chill", 0);
